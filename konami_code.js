dd334d8041889beca647fc8bbb9a861d47a82e5f
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-09-18 11:21:02 +0100

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,32 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'b',
+  'a'
+]
 
-function init() {
-  // your code here
+function init () {
+  let index = 0
+
+  document.body.addEventListener('keydown', event => {
+    const key = event.key
+
+    if (key === codes[index]) {
+      index++
+
+      if (index === codes.length) {
+        alert('Hurrey!')
+
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
 }
